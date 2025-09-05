9a137cd76be2c4eafea4c2d347c694edbe2a4b94
Manikanta Thummuri <postbox5827@gmail.com>
2025-09-05 17:58:24 +0530

    update: initialise service

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const path = require('path');
+const userRoutes = require('./src/routes/userRoutes.js');
+require('./src/cron/cronJob.js');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'subscribe.html'));
+})
+
+app.use('/api', userRoutes);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
